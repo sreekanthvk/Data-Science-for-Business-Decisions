28166cbe26c3ae08d912ceeb896c8129b2efa3a2
Sreekanth V K <sreekanth.vk@outlook.com>
2021-01-28 11:54:48 +0530

    Update plots intro
    Updated

--- R/plots-intro.R
+++ R/plots-intro.R
@@ -1,4 +1,5 @@
 ?datasets
+library( help = "ISLR")
 library(help = "datasets")
 ?iris
 
@@ -10,16 +11,21 @@ head(iris)
 str(iris)
 
 ?is.na
+length(is.na(iris))
+sum(is.na(iris))
 iris[is.na(iris),]
 
 summary(iris)
 
 hist(iris$Sepal.Length)
 hist(iris$Petal.Length)
+hist(iris$Petal.Length[iris$Species != "setosa"])
 
 plot(iris$Species)             # plotting categorical values
 plot(iris$Sepal.Length)        # plotting numerical values
+plot(iris$Sepal.Length, col = iris$Species) 
 plot(iris$Petal.Length, col = iris$Species)
+
 plot(iris$Petal.Length, iris$Petal.Width)
 
 plot(iris$Petal.Length, iris$Petal.Width, col = iris$Species)
