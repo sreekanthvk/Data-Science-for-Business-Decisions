7b4961995dfb1a464c9c77a43300880cac484acc
Sreekanth V K <sreekanth.vk@outlook.com>
2021-02-04 11:19:15 +0530

    Create introduction to python
    Initial version

---
+++ Python/introduction_to_python.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import math 
+
+variable1 = 2
+variable2 = 3
+variable3 = variable1 + variable2
+variable3
+
+variable1 = "Hello"
+variable2 = " World"
+variable3 = variable1 + variable2
+variable3
+
+sum([variable1,variable2])
+
+math.pow(variable1,variable2)
+
+def sum_of_variables (var1, var2):
+    return(var1+var2)
+
+sum_of_variables(1,2)
